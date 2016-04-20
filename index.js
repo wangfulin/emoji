9edb22a2d67713f581b6ae9a87c17c2ffb08f0fc
wangfulin <1622913826@qq.com>
2016-04-20 15:52:19 +0800

    add universal support

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var Emoji = requrie('./src/emoji.js');
+var Emoji = require('./src/emoji.js');
 
-module.exports = Emoji;
+(function(window, Emoji){
+	if(typeof module !== 'undefined'){
+		module.exports = Emoji;
+	}else if(typeof define == 'function' && typeof define.amd == 'object'){
+		define('Emoji', [], Emoji);
+	}else{
+		global['Emoji'] = Emoji;
+	}
+})(window, Emoji);
